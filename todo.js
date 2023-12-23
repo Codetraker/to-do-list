7f9ee27b6935d269699025b03c4369c6ae65830e
Codetraker <swarajbibhuprasad@gmail.com>
2023-12-24 00:26:39 +0530

    major change

--- todo.js
+++ todo.js
@@ -9,13 +9,12 @@ window.addEventListener('DOMContentLoaded',getData)
 form.addEventListener('submit',addIt);
 
 function getData(){
-    axios.get('https://crudcrud.com/api/9ad2677f5c90423ab089492278187d96/tododata')
+    axios.get('https://crudcrud.com/api/71c8535e94b748f9af8eb0ed59bfd314/tododata')
         .then((response) =>{
             response.data.forEach((element) => {
-                let id = element._id;
-                displayToDo(element,id);
+                displayToDo(element.title,element.descript,element._id,element.completed);
             });
-    })
+    }).catch((error)=>{console.log("This is a error in getData" + error)});
 }
 
 function addIt(e){
@@ -28,31 +27,29 @@ function addIt(e){
         descript : descript.value,
         completed : false
     }
-    axios.post('https://crudcrud.com/api/9ad2677f5c90423ab089492278187d96/tododata',userData)
+    axios.post('https://crudcrud.com/api/71c8535e94b748f9af8eb0ed59bfd314/tododata',userData)
         .then((response)=>{
-            remainlist.innerHTML="";
-            donelist.innerHTML="";
-            getData();
-        }).catch((error)=>{console.log("This is a error in addIt")});
+            displayToDo(response.data.title, response.data.descript, response.data._id, response.data.completed);
+        }).catch((error)=>{console.log("This is a error in addIt" + error)});
 
     e.target.reset();
 }
 
-function displayToDo(response,id){
+function displayToDo(title,descript,id,completed){
     let newLi = document.createElement('li');
-    newLi.innerHTML = `To-do Detail:-${response.title}-Detail:-${response.descript}`;
+    newLi.innerHTML = `To-do Detail:-${title}-Detail:-${descript}`;
 
-    if(response.completed == false){
+    if(completed == false){
         let addBtn = document.createElement('button');
         addBtn.appendChild(document.createTextNode('Done'));
-        addBtn.onclick = function(){
-            editCompleted(id);
+        addBtn.onclick = function(e){
+            editCompleted(title,descript,id,e);
         }
 
         let deleteBtn = document.createElement('button');
         deleteBtn.appendChild(document.createTextNode('Delete'));
-        deleteBtn.onclick = function(){
-            deleteItem(id);
+        deleteBtn.onclick = function(e){
+            deleteItem(id,e);
         }
 
         newLi.appendChild(addBtn);
@@ -61,34 +58,43 @@ function displayToDo(response,id){
     }else{
         let deleteBtn = document.createElement('button');
         deleteBtn.appendChild(document.createTextNode('Delete'));
-        deleteBtn.onclick = function(){
-            deleteItem(id);
+        deleteBtn.onclick = function(e){
+            deleteItem(id,e);
         }
         newLi.appendChild(deleteBtn);
         donelist.appendChild(newLi);
     }
 }
 
-function editCompleted(id){
-    axios.patch(`https://crudcrud.com/api/9ad2677f5c90423ab089492278187d96/tododata/${id}`,{
-        completed:'true'
-    }).then((response)=>{
-        remainlist.innerHTML="";
-        donelist.innerHTML="";
-        getData();
+function editCompleted(title,descript,id,e){
+    axios.put(`https://crudcrud.com/api/71c8535e94b748f9af8eb0ed59bfd314/tododata/${id}`,{
+        title : title,
+        descript : descript,
+        completed : true
+    }).then((response)=>{ 
+        displayToDo(title,descript,id,true);
+        let li = e.target.parentElement;
+        remainlist.removeChild(li);
+        
     }).catch((error)=>{
-        console.log("Error in editcompleted")
+        console.log("Error in editcompleted "+error)
     });
 }
 
-function deleteItem(id){
-    axios.delete(`https://crudcrud.com/api/9ad2677f5c90423ab089492278187d96/tododata/${id}`,{
-        completed:true
+function deleteItem(id,e){
+    axios.delete(`https://crudcrud.com/api/71c8535e94b748f9af8eb0ed59bfd314/tododata/${id}`,{
+
     }).then((response)=>{
-        remainlist.innerHTML="";
-        donelist.innerHTML="";
-        getData();
+        let li = e.target.parentElement;
+        let parentList = li.parentElement;
+        
+        if(parentList === remainlist){
+            remainlist.removeChild(li);
+        }else if(parentList === donelist){
+            donelist.removeChild(li);
+        }
+
     }).catch((error)=>{
-        console.log("Error in deleteBtn")
+        console.log("Error in deleteBtn " + error)
     });
 }
