33a98567393517a17f77fdbb75b7653326fe37fa
Codetraker <swarajbibhuprasad@gmail.com>
2023-12-26 14:07:53 +0530

    using async await with try and catch

--- todo.js
+++ todo.js
@@ -8,16 +8,19 @@ let donelist = document.getElementById('dlist');
 window.addEventListener('DOMContentLoaded',getData)
 form.addEventListener('submit',addIt);
 
-function getData(){
-    axios.get('https://crudcrud.com/api/71c8535e94b748f9af8eb0ed59bfd314/tododata')
-        .then((response) =>{
-            response.data.forEach((element) => {
-                displayToDo(element.title,element.descript,element._id,element.completed);
-            });
-    }).catch((error)=>{console.log("This is a error in getData" + error)});
+async function getData(){
+    try{
+        const response = await axios.get('https://crudcrud.com/api/5fed7c0f20ec47739dde1e60cecd7595/tododata');
+        response.data.forEach((element) => {
+        displayToDo(element.title,element.descript,element._id,element.completed);
+        });
+    }catch(error){
+        console.log(error);
+    }
+    
 }
 
-function addIt(e){
+async function addIt(e){
     e.preventDefault();
     
     let userData = {
@@ -25,10 +28,12 @@ function addIt(e){
         descript : descript.value,
         completed : false
     }
-    axios.post('https://crudcrud.com/api/71c8535e94b748f9af8eb0ed59bfd314/tododata',userData)
-        .then((response)=>{
-            displayToDo(response.data.title, response.data.descript, response.data._id, response.data.completed);
-        }).catch((error)=>{console.log("This is a error in addIt" + error)});
+    try{
+        const response = await axios.post('https://crudcrud.com/api/5fed7c0f20ec47739dde1e60cecd7595/tododata',userData); 
+        displayToDo(response.data.title, response.data.descript, response.data._id, response.data.completed);
+    }catch(error){
+        console.log(error);
+    }
 
     e.target.reset();
 }
@@ -64,35 +69,34 @@ function displayToDo(title,descript,id,completed){
     }
 }
 
-function editCompleted(title,descript,id,e){
-    axios.put(`https://crudcrud.com/api/71c8535e94b748f9af8eb0ed59bfd314/tododata/${id}`,{
-        title : title,
-        descript : descript,
-        completed : true
-    }).then((response)=>{ 
+async function editCompleted(title,descript,id,e){
+    try{
+        const response = await axios.put(`https://crudcrud.com/api/5fed7c0f20ec47739dde1e60cecd7595/tododata/${id}`,{
+            title : title,
+            descript : descript,
+            completed : true
+        }); 
         displayToDo(title,descript,id,true);
         let li = e.target.parentElement;
         remainlist.removeChild(li);
-        
-    }).catch((error)=>{
-        console.log("Error in editcompleted "+error)
-    });
+    }catch(error){
+        console.log(error);
+    }        
 }
 
-function deleteItem(id,e){
-    axios.delete(`https://crudcrud.com/api/71c8535e94b748f9af8eb0ed59bfd314/tododata/${id}`,{
-
-    }).then((response)=>{
-        let li = e.target.parentElement;
-        let parentList = li.parentElement;
-        
-        if(parentList === remainlist){
-            remainlist.removeChild(li);
-        }else if(parentList === donelist){
-            donelist.removeChild(li);
-        }
-
-    }).catch((error)=>{
-        console.log("Error in deleteBtn " + error)
-    });
+async function deleteItem(id,e){
+    try{
+        const response = await axios.delete(`https://crudcrud.com/api/5fed7c0f20ec47739dde1e60cecd7595/tododata/${id}`);
+            let li = e.target.parentElement;
+            let parentList = li.parentElement;
+                
+            if(parentList === remainlist){
+                remainlist.removeChild(li);
+            }else if(parentList === donelist){
+                donelist.removeChild(li);
+            }
+    }catch(error){
+        console.log(error);
+    }
+    
 }
